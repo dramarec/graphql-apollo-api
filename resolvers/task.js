f82e1fcba4ce5106e1df75afcaaae4ea1f9d786d
Oleg Dramaretskyi <dramarec@gmail.com>
2021-06-16 10:53:04 +0300

    added createTask mutation

--- resolvers/task.js
+++ resolvers/task.js
@@ -1,26 +1,36 @@
-const uuid = require('uuid');
+const { combineResolvers } = require('graphql-resolvers');
 
-const { users, tasks } = require('../constants');
+const Task = require('../database/models/task');
+const User = require('../database/models/user');
+const { isAuthenticated } = require('./middleware');
 
 module.exports = {
-  Query: {
-    tasks: () => {
-      return tasks;
+    Query: {
+        tasks: () => {
+            return tasks;
+        },
+        task: (_, { id }) => {
+            return tasks.find(task => task.id === id)
+        },
     },
-    task: (_, { id }) => {
-      return tasks.find(task => task.id === id)
+    Mutation: {
+        createTask: combineResolvers(isAuthenticated, async (_, { input }, { email }) => {
+            try {
+                const user = await User.findOne({ email });
+                const task = new Task({ ...input, user: user.id });
+                const result = await task.save();
+                user.tasks.push(result.id);
+                await user.save();
+                return result;
+            } catch (error) {
+                console.log(error);
+                throw error;
+            }
+        }),
     },
-  },
-  Mutation: {
-    createTask: (_, { input }) => {
-      const task = { ...input, id: uuid.v4() };
-      tasks.push(task);
-      return task;
+    Task: {
+        user: ({ userId }) => {
+            return users.find(user => user.id === userId)
+        }
     }
-  },
-  Task: {
-    user: ({ userId }) => {
-      return users.find(user => user.id === userId)
-    }
-  }
 }
